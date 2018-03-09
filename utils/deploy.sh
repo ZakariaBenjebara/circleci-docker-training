67da801629958fe2a9f8a775f2147d1575bf15aa
Zakaria Benjebara <zakaria.benjebara@nestle.com>
2018-03-09 15:55:16 +0000

    update config.yml to support deploy.sh script

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -2,6 +2,12 @@
 
 set -ex
 
+REGISTRY=$1
+IMAGE_NAME=$2
+DOCKER_USER=$3
+DOCKER_PASS=$4
+TAG=$5
+
 docker build -t ${REGISTRY}/${IMAGE_NAME}:$TAG .
 docker login -u $DOCKER_USER -p $DOCKER_PASS         
 docker tag ${REGISTRY}/${IMAGE_NAME}:$TAG ${REGISTRY}/${IMAGE_NAME}:latest
